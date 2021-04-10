e54404bc2d1ead44acbd1ef158b04c522108feda
Valerie Ricot <valeriericot@gmail.com>
2021-04-10 19:02:02 -0400

    Added some classes

--- front-page.php
+++ front-page.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+<div class="container mt-3">
+  <div class="row">
+    <div class="col-12">
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
           <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -7,24 +10,24 @@
           </div>
           <div class="carousel-inner">
             <div class="carousel-item active">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/Sahara-Promo.jpeg" class="d-block w-100 carousel-img" alt="Sahara">
-              <div class="carousel-caption d-none d-md-block">
-                <h5>First slide label</h5>
-                <p>Some representative placeholder content for the first slide.</p>
+              <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/Sahara-Promo.jpeg" class="d-block w-100 carousel-img" alt="Sahara">
+              <div class="carousel-caption d-none d-md-block"></a>
+                <h5>SAHARA</h5>
+                <p>New Global Edition of Sahara Out Now On Amazon!</p>
               </div>
             </div>
             <div class="carousel-item">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/who-killed-the-poet.jpeg" class="d-block w-100 carousel-img" alt="Angella's YouTube Channel">
-              <div class="carousel-caption d-none d-md-block">
-                <h5>Second slide label</h5>
-                <p>Some representative placeholder content for the second slide.</p>
+              <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/who-killed-the-poet.jpeg" class="d-block w-100 carousel-img" alt="Who Killed The Poet">
+              <div class="carousel-caption d-none d-md-block"></a>
+                <h5>WHO KILLED THE POET</h5>
+                <p>Check Out My Latest Poetry Collection, Now On Amazon!</p>
               </div>
             </div>
             <div class="carousel-item">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/YouTube-thumb.png" class="d-block w-100 carousel-img" alt="Angella Ricot">
+              <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/YouTube-thumb.png" class="d-block w-100 carousel-img" alt="Angella's Book Hotspot"></a>
               <div class="carousel-caption d-none d-md-block">
-                <h5>Third slide label</h5>
-                <p>Some representative placeholder content for the third slide.</p>
+                <h5>ANGELLA'S BOOK HOTSPOT</h5>
+                <p>Check Out My YouTube Channel!</p>
               </div>
             </div>
           </div>
@@ -38,9 +41,12 @@
           </button>
         </div>
       <div class="container">
+    </div>
+  </div>
+</div>
+
         <section class="about-section">
-            
-          <h3 class="mt-3 section-heading">About</h3>
+          <h3 class="section-heading">About</h3>
           
               <div class="row">
                   <div class="col-md-6">
@@ -50,10 +56,10 @@
                       <p class="about-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni amet voluptas quidem culpa. Nesciunt sint, fuga veniam quasi aperiam necessitatibus possimus, asperiores blanditiis, ut incidunt vero deserunt natus reprehenderit ipsam.</p>
                   </div>
               </div>
-          
           </section>
+
           <section class="book-section">
-          <h3 class="mt-3 section-heading">Books</h3>
+          <h3 class="section-heading">Books</h3>
           <!-- <div class="container"> -->
               <div class="row">
                   <div class="col-md-6">
@@ -64,30 +70,30 @@
                                   <img src="<?php echo get_template_directory_uri(); ?>/img/Sahara.png" alt="Sahara" class="book-img">
                               </div>
                               <div class="col-sm-6">
-                                  <h4>Sahara</h4>
+                                  <h4 class="book-title">Sahara</h4>
                                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptas omnis et excepturi ipsum quaerat, asperiores maxime corporis quam earum perferendis facere modi ab possimus ad! Quisquam numquam deserunt sequi.</p>
                               </div>
                           </div>
                       </div>
                   </div>
                   <div class="col-md-6">
-                      <h3 class="section-subheading mb-3">Poetry</h3>
+                      <h3 class="section-subheading mb-3 poetry-heading">Poetry</h3>
                       <div class="container">
-                          <div class="row">
+                          <div class="row mb-5">
                               <div class="col-sm-6">
                                   <img src="<?php echo get_template_directory_uri(); ?>/img/Who-Killed-The-Poet-Book.png" alt="Who Killed The Poet" class="book-img">
                               </div>
                               <div class="col-sm-6 mb-5">
-                                  <h4>Who Killed The Poet?</h4>
+                                  <h4 class="book-title">Who Killed The Poet?</h4>
                                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptas omnis et excepturi ipsum quaerat, asperiores maxime corporis quam earum perferendis facere modi ab possimus ad! Quisquam numquam deserunt sequi.</p>
                               </div>
                           </div>
-                          <div class="row">
+                          <div class="row new-row">
                               <div class="col-sm-6">
                                   <img src="<?php echo get_template_directory_uri(); ?>/img/Mirror-of-Souls.png" alt="Mirror of Souls" class="book-img">
                               </div>
                               <div class="col-sm-6">
-                                  <h4>Mirror of Souls</h4>
+                                  <h4 class="book-title">Mirror of Souls</h4>
                                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptas omnis et excepturi ipsum quaerat, asperiores maxime corporis quam earum perferendis facere modi ab possimus ad! Quisquam numquam deserunt sequi.</p>
                               </div>
                           </div>
@@ -106,6 +112,9 @@
             </div>
         </section>
         <section class="blog-section">
+        <!-- </div> -->
+            <h3 class="section-heading">Blog</h3>
+          <!-- <div class="container"> -->
         <?php 
                 
                 $args = array(
@@ -120,9 +129,6 @@
                 
                 
                 ?> 
-          <!-- </div> -->
-          <h3 class="mt-3 section-heading">Blog</h3>
-          <!-- <div class="container"> -->
             <div class="row">
                 <div class="col-12">
                   <div class="container-fluid blog-container">
@@ -152,11 +158,14 @@
         </section>
         <section class="connect-section">
           <h2 class="section-heading">connect</h2>
+          <div class="connect-paragraph">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores qui distinctio ducimus nesciunt voluptas ipsam magni non ipsum voluptate tempore et aperiam veritatis quas quibusdam quae consequuntur doloribus, at eos.</p>
+          </div>
                         <div class="containter-fluid">
                             <div class="col-lg-8 offset-lg-2">
                             <div class="container-fluid mb-3 icons">
                             <!-- <a href="#" class="font-awesome" id="github" target="_blank"><i class="fa fa-github"></i> GitHub</a> -->
-                            <a href="#" target="_blank"><i class="fa fa-linkedin" id="linkedin"></i> LinkedIn</a>
+                            <a href="#" target="_blank"><i class="fa fa-linkedin linkedin" id="linkedin"></i> LinkedIn</a>
                         </div>
                             <div class="row">
                                 <div class="col-md-6">
