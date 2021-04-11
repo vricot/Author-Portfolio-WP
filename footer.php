4738b43f7613a2d6f148b26ce279f43494d74b46
Valerie Ricot <valeriericot@gmail.com>
2021-04-11 17:42:29 -0400

    Forced footer to bottom of page with div wrapper

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
+<div class="footer-margin" style="height: 75px;">
 <footer>
-    <p class="fixed-bottom">&copy; Valerie Ricot 2021</p>
+    <p>&copy; Valerie Ricot 2021</p>
 </footer>
+</div>
+
 
 <?php wp_footer(); ?>
 
