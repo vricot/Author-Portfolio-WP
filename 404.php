84b39ac6722c45a8ac7147794b6d879fade70530
Valerie Ricot <valeriericot@gmail.com>
2021-04-11 19:54:45 -0400

    Initial Commit

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-10 offset-lg-1">
+            <h2 class="section-heading error-page-heading">
+                Sorry Love, you might be lost!
+            </h2>
+            <img class="error-img" src="http://source.unsplash.com/640x480/?writing">
+        </div>
+    </div>
+</div>
+
+<div>
+    <a class="btn btn-primary return-btn" href="<?php echo site_url('') ?>">Click here to return to my home page!</a>
+</div>
+
+<?php get_footer(); ?>
